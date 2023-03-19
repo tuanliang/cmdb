12d81997195c7f01627934031147be4091e895ac
shiyi <2416976257@qq.com>
2023-03-19 16:36:26 +0800

    补充sql和表

--- docs/schema/tables.sql
+++ docs/schema/tables.sql
@@ -10,3 +10,55 @@ CREATE TABLE IF NOT EXISTS `books` (
   KEY `idx_name` (`name`) USING BTREE COMMENT '用于书名搜索',
   KEY `idx_author` (`author`) USING BTREE COMMENT '用于作者搜索'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE IF NOT EXISTS `resource` (
+  `id` char(64) NOT NULL COMMENT '全局唯一Id, 直接使用个云商自己的Id',
+  `resource_type` tinyint(2) NOT NULL COMMENT '资源类型',
+  `vendor` tinyint(1) NOT NULL COMMENT '厂商',
+  `region` varchar(64) NOT NULL COMMENT '地域',
+  `zone` varchar(64) NOT NULL COMMENT '区域',
+  `create_at` bigint(13) NOT NULL COMMENT '创建时间',
+  `expire_at` bigint(13) DEFAULT NULL COMMENT '过期时间',
+  `category` varchar(64) NOT NULL COMMENT '种类',
+  `type` varchar(120) NOT NULL COMMENT '规格',
+  `name` varchar(255) NOT NULL COMMENT '名称',
+  `description` varchar(255) DEFAULT NULL COMMENT '描述',
+  `status` varchar(255) NOT NULL COMMENT '服务商中的状态',
+  `update_at` bigint(13) DEFAULT NULL COMMENT '更新时间',
+  `sync_at` bigint(13) DEFAULT NULL COMMENT '同步时间',
+  `sync_accout` varchar(255) DEFAULT NULL COMMENT '同步账号',
+  `public_ip` varchar(64) DEFAULT NULL COMMENT '公网IP',
+  `private_ip` varchar(64) DEFAULT NULL COMMENT '内网IP',
+  `pay_type` varchar(255) DEFAULT NULL COMMENT '实例付费方式',
+  `describe_hash` varchar(255) NOT NULL COMMENT '描述数据Hash',
+  `resource_hash` varchar(255) NOT NULL COMMENT '基础数据Hash',
+  `secret_id` varchar(64) NOT NULL COMMENT '关联的同于同步的secret id',
+  `domain` varchar(255) NOT NULL COMMENT '资源所属域',
+  `namespace` varchar(255) NOT NULL COMMENT '资源所属空间',
+  `env` varchar(255) NOT NULL COMMENT '资源所属环境',
+  `usage_mode` tinyint(2) NOT NULL COMMENT '资源使用方式',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_name` (`name`) USING BTREE,
+  KEY `idx_status` (`status`) USING BTREE,
+  KEY `idx_private_ip` (`public_ip`) USING BTREE,
+  KEY `idx_public_ip` (`public_ip`) USING BTREE,
+  KEY `idx_domain` (`domain`) USING HASH,
+  KEY `idx_namespace` (`namespace`) USING HASH,
+  KEY `idx_env` (`env`) USING HASH
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源基础信息,所有资源的公共信息, 用于全局解索';
+
+CREATE TABLE IF NOT EXISTS `resource_tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '标签Id',
+  `t_key` varchar(255) NOT NULL COMMENT '标签的名称',
+  `t_value` varchar(255) NOT NULL COMMENT '标签的值',
+  `description` varchar(255) NOT NULL COMMENT '值的描述信息',
+  `resource_id` varchar(64)  NOT NULL COMMENT '标签关联的资源Id',
+  `weight` int(11) NOT NULL COMMENT '标签权重',
+  `type` tinyint(4) NOT NULL COMMENT '标签类型',
+  `create_at` bigint(13) NOT NULL COMMENT '标签创建时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_id` (`t_key`,`t_value`,`resource_id`) COMMENT '一个资源同一个key value只允许有一对',
+  KEY `idx_key` (`t_key`) USING HASH,
+  KEY `idx_value` (`t_value`) USING BTREE,
+  KEY `idx_resource_id` (`resource_id`) USING HASH
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COMMENT='资源标签';
